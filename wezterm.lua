741cf2f1e6ede2bd9288c5f12d5a69eb35304b2f
IronCubeStudios <orangutanggreen722@gmail.com>
2025-05-09 19:07:03 -0400

    wezterm config

---
+++ wezterm.lua
@@ -0,0 +1,13 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+return {
+  window_background_image = "C:\\Users\\orang\\AppData\\Local\\nvim",
+  window_background_opacity = 0.90,
+  text_background_opacity = 0.6,
+  enable_scroll_bar = false,
+  font = wezterm.font("FiraCode Nerd Font"),
+  font_size = 12.5,
+  color_scheme = "Catppuccin Mocha",
+}
+
